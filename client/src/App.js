323b92305fdd30987c8513e5fe12e5ca3bfa8706
Abhishek Jakhar <abhishekjakhar.11@gmail.com>
2022-07-30 00:53:44 +0530

    fix: update App.js import

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Setting from "./pages/setting/Setting";
 import Login from "./pages/login/Login";
-import Layout from "./components/layout/Layout";
+import Layout from "./components/Layout/Layout";
 
 import axios from "axios";
 
@@ -14,7 +14,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useUserDataContext } from "./context/UserContext";
 import { useUpdateModeContext } from "./context/UpdateModeContext";
-
 
 require("dotenv").config();
 function App() {
@@ -40,42 +39,48 @@ function App() {
     const fetchErrorUrl = async () => {
       const ans = searchParams.get("error");
       if (ans === "unauthorized") {
-
-        localStorage.setItem('snackbar', true)
+        localStorage.setItem("snackbar", true);
         logout({ returnTo: window.location.origin });
         // Question: make pop up here
       }
-    }
+    };
     fetchErrorUrl();
   }, []);
-
 
   function scrollToHomeHandler() {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: 'smooth'
-    })
+      behavior: "smooth",
+    });
   }
 
-
   return (
-    <Layout scrollHomeHandler={scrollToHomeHandler} >
+    <Layout scrollHomeHandler={scrollToHomeHandler}>
       <Routes>
         {/* Login */}
-        <Route path="/login" element={user ?
-          <div >
-            <Home />
-          </div> : <Login />} />
+        <Route
+          path="/login"
+          element={
+            user ? (
+              <div>
+                <Home />
+              </div>
+            ) : (
+              <Login />
+            )
+          }
+        />
 
         {/* Home */}
         <Route
           path="/"
           element={
             // Question: weird position when clicked
-            <div >
+            <div>
               <Home />
-            </div>}
+            </div>
+          }
         />
 
         {/* Write */}
