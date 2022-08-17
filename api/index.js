183cbfc82dd6b3029701c0ac33c2214ea3a7c397
sherly <sharton1@asu.edu>
2022-08-17 14:59:27 -0400

    make it better

--- api/index.js
+++ api/index.js
@@ -158,9 +158,9 @@ app.use("/api/categories", categoriesRoute);
 
 
 if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-    app.use(express.static("client / build"));
+    app.use(express.static("client/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname + "/client/build / index.html"));
+        res.sendFile(path.join(__dirname + "/client/build/index.html"));
     });
 }
 
