6447b2c90d65bdfb0435ee3467bb492b5db8702a
sherly <sharton1@asu.edu>
2022-09-03 17:44:45 -0400

    add likes

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-const baseUrl = "http://localhost:3000/"
-// const baseUrl = "https://hartono-s.herokuapp.com/"
+// const baseUrl = "http://localhost:3000/"
+const baseUrl = "https://hartono-s.herokuapp.com/"
 
-export const apiUrl = "http://localhost:4000/api/"
-// export const apiUrl = "https://hartono-s.herokuapp.com/api/"
+// export const apiUrl = "http://localhost:4000/api/"
+export const apiUrl = "https://hartono-s.herokuapp.com/api/"
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl
