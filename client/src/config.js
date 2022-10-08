77e9dfff2a3b33c49ba1390323ece728e0fda785
sherly <sharton1@asu.edu>
2022-10-08 11:30:37 +0700

    test localhost

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,11 @@
 import axios from "axios"
 
-const baseUrl = "http://localhost:3000/"
 // const baseUrl = "https://hartono-s.herokuapp.com/"
-
-export const apiUrl = "http://localhost:4000/api/"
 // export const apiUrl = "https://hartono-s.herokuapp.com/api/"
+
+
+const baseUrl = "http://localhost:3000/"
+export const apiUrl = "http://localhost:4000/api/"
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl
