3d3bd3721fd27a00ab09a58657e1e28ae068de3f
sherly <sharton1@asu.edu>
2022-12-06 15:13:51 +0700

    dev mode

--- client/src/config.js
+++ client/src/config.js
@@ -4,8 +4,8 @@ const baseUrl = "https://hartono-s.herokuapp.com/"
 export const apiUrl = "https://hartono-s.herokuapp.com/api/"
 
 
-// const baseUrl = "http://localhost:3000/"
-// export const apiUrl = "http://localhost:4000/api/"
+const baseUrl = "http://localhost:3000/"
+export const apiUrl = "http://localhost:4000/api/"
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl
